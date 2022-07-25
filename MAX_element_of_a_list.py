5262e744e967a294b86d3643dd44faf21bd91694
Atul Gupta <atulguptag111@gmail.com>
2022-07-26 01:16:28 +0530

    sixth comment

---
+++ MAX_element_of_a_list.py
@@ -0,0 +1,8 @@
+#creating a list which stores some values
+list1 = [5, 10, 15, 20, 51, 50, 25, 30, 35, 40]
+
+#in python, there is a built-in function 'max()'.
+#you can use it, to find the largest number in a list.
+max_list1 = max(list1)
+
+print("Max value of a list :", max_list1) 
