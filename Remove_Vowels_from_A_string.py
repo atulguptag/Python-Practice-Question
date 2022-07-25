d5e6325e15a48db693b2e9480bc8de322319223b
Atul Gupta <atulguptag111@gmail.com>
2022-07-26 00:19:58 +0530

    fifth comment

--- Remove_Vowels_from_A_string.py
+++ Remove_Vowels_from_A_string.py
@@ -1,24 +1,7 @@
-# Slicing method to remove the vowels from a string
-strg = str(input("Enter a string : "))
-
-vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-
-i = 0 
-
-for c in strg:
-    if c in vowels:
-        strg = str[:1] + str[i+1:]
-        i = i - 1
-
-    i += 1
-    
-print("After removing Vowels : ", strg)
-
-
 # Simple implementation method to remove the vowels from a string
 #take input from user
 String = str(input("Enter a string : "))
-
+print("\n")
 #initialize new empty string
 result = str()
 
@@ -33,18 +16,7 @@ for i in String:
 
 #Check for if the result is not null
 if len(result) == 0:
-    print("No vowel found in " + String)
+    print("No vowel found in : " + String)
 
 else:
     print("After removing Vowels : " + result)
-
-
-# Regular Expression(re) Method to remove the vowels from a string
-
-# First import the module for regular expression (re)
-import re
-def rem_vowel(string):
-    return (re.sub("[aeiouAEIOU]","",string))           
-
-string = str(input("Enter a string : "))
-print(rem_vowel(string))
