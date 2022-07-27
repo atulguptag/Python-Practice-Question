bc4effeda1a3d7f6864f8d0904f8fdd4689fa63f
Atul Gupta <atulguptag111@gmail.com>
2022-07-27 10:33:12 +0530

    Adding some comments in code

--- Anagrams_program.py
+++ Anagrams_program.py
@@ -1,23 +1,32 @@
 from operator import le
 from numpy import sort
 
+#define main() function
 def main():
+
+    #takeing user input
     str_s1 = str(input("Enter first string : "))
     str_s2 = str(input("Enter second string : "))  
     print("\n", end = '')    
 
+    #check lengths of both string
     len1 = len(str_s1)
     len2 = len(str_s2)
 
+    #printing lengths
     print("Length of len1 : ", len1)
     print("Length of len2 : ", len2)
 
+    #checking if lengths are equal to each other
     if len1 == len2:
         l1 = (sorted(str_s1, key = len))
         l2 = (sorted(str_s2, key = len))
 
+        #printing sorted lengths
         print("Sorted Str_1 = ", l1)
         print("Sorted Str_2 = ", l2)
+
+        #checking if strings are equals to each other
         if str_s1 == str_s2:
             print("\nBoth are in Anagrams", end = '')
             print("\n", end = '')
@@ -29,5 +38,6 @@ def main():
         print("\nBoth are not Anagrams", end = '')
         print("\n", end = '')
 
+#driven code
 if __name__ == "__main__":
     main()
