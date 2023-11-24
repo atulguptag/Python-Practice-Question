0a5787b8aae136211707dde66ee353f21fab92ce
Atul Gupta <atulguptag111@gmail.com>
2023-11-24 23:47:24 +0530

    Update StringFromRank.py

--- TCS Codevita Problems Answers/String From Rank/StringFromRank.py
+++ TCS Codevita Problems Answers/String From Rank/StringFromRank.py
@@ -1,23 +1,19 @@
+import itertools
+
+def generate_permutations(length):
+    alphabets = [chr(ord('a') + i) for i in range(26)]
+    all_permutations = list(itertools.permutations(alphabets, length))
+    sorted_permutations = sorted(all_permutations)
+    return sorted_permutations
+
 def find_string(rank, length):
-    import math
-    alphabets = 'abcdefghijklmnopqrstuvwxyz'
-    result = ''
-    while length > 0:
-        q, r = divmod(rank - 1, math.factorial(length-1))
-        result += alphabets[q % len(alphabets)] 
-        alphabets = alphabets[:q] + alphabets[q+1:]
-        rank = r + 1
-        length -= 1
-    return result
+    sorted_permutations = generate_permutations(length)
+    required_permutation = sorted_permutations[rank-1]
+    return ''.join(required_permutation)
+
 
 rank_input = int(input())
 length_input = int(input())
 
 result_output = find_string(rank_input, length_input)
 print(result_output)
-
-"""
-NOTE:- Wrong output for second input - 
-29
-2
-"""
